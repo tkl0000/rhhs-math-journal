2348e3a23709c9bb4a90b9f73d971788204c31b1
thomas <thomasli2025@gmail.com>
2024-08-16 14:42:16 -0400

    journal

---
+++ src/pages/JournalPage.tsx
@@ -0,0 +1,24 @@
+// src/HomePage.js
+// import React from 'react';
+
+import Banner from '../components/Banner.tsx';
+// import { MathJax } from 'better-react-mathjax';
+// import ParallaxEquations from '../components/ParallaxEquations.tsx';
+// import anime from 'animejs'
+// import React, { useState, useEffect, useRef, ReactNode } from 'react';
+
+const JournalPage = () => {
+
+    return (
+        <main className="container mx-auto">
+            <div className="text-slate-200 text-xl">
+                Lorem Ipsum
+                {/* <p>Jajjajjajajajjajja</p>
+            <MathJax>{"\\(\\frac{1x}{2x} \\neq 4 \\)"}</MathJax> */}
+                {/* <ParallaxEquations /> */}
+            </div>
+        </main>
+    );
+};
+
+export default JournalPage;
