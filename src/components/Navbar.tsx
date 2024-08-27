ed38c558912c1b6c74b4ae2fec1cd3c0533a4069
tkl0000 <thomasli2025@gmail.com>
2024-08-26 20:26:38 -0400

    responsive navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
-import NavbarButton from './NavbarButton';
+import { Link } from 'react-router-dom';
 
 const base_url = import.meta.env.VITE_BASE_URL;
 
 const Navbar: React.FC = () => {
   return (
     <nav className="bg-gray-800 p-4 sticky top-0 w-full z-10">
-      <div className="container mx-auto flex justify-center items-center">
-        <div className="flex space-x-8">
+      <div className="container mx-auto flex justify-between items-center">
+        {/* Logo or Brand Name (optional) */}
+        <div className="text-slate-200 text-xl font-bold hidden sm:block">
+        </div>
+        <div className="flex overflow-x-auto space-x-4 sm:space-x-8 scrollbar-hide">
           <NavbarButton title="home" destination={`${base_url}`} />
           <NavbarButton title="articles" destination={`${base_url}articles`} />
           <NavbarButton title="journal" destination={`${base_url}journal`} />
@@ -19,15 +22,17 @@ const Navbar: React.FC = () => {
   );
 }
 
-// function Navbar() {
-//   return (
-//     <div className="min-h-screen bg-gray-100 flex items-center justify-center">
-//       <div className="bg-white p-8 rounded shadow-md">
-//         <h1 className="text-2xl font-bold mb-4">Hello, Tailwind CSS!</h1>
-//         <p className="text-gray-700">This is a simple example of using Tailwind CSS with React.</p>
-//       </div>
-//     </div>
-//   );
-// }
+interface NavbarButtonProps {
+  title: string;
+  destination: string;
+}
+
+const NavbarButton: React.FC<NavbarButtonProps> = ({ title, destination }) => {
+  return (
+    <Link to={destination} className="text-slate-200 hover:text-white px-2 sm:px-3 py-2 rounded-md text-sm sm:text-xl font-medium whitespace-nowrap">
+      <h1>{title}</h1>
+    </Link>
+  );
+}
 
 export default Navbar;
